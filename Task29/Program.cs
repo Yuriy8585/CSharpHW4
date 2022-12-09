1fae81478708b3dc809423dc930d2a8c382527c2
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-09 15:40:49 +0300

    mistakes

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,7 +3,18 @@
 Console.Clear();
 Console.WriteLine("Enter number = ");
 
-int i = int.Parse(Console.ReadLine()!);
-var nums = i.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+//int i = int.Parse(Console.ReadLine()!);
+//var nums = i.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
 
-Console.WriteLine(i);
+//Console.WriteLine(nums);
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+    Console.Write($"{array[i]}");
+    }
+Console.WriteLine();
+}
+PrintArray(arr);
