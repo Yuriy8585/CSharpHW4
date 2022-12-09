f515e91588264d5be01789b1566dcf3a5fd91828
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-09 15:19:05 +0300

    mistakes

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,3 +1,9 @@
 ﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
 
+Console.Clear();
+Console.WriteLine("Enter number = ");
 
+int i = int.Parse(Console.ReadLine()!);
+var nums = i.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+
+Console.WriteLine(i);
