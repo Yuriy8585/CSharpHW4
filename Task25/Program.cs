e7442c78345eba325954c575fef379390367c0df
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-09 14:12:38 +0300

    mistakes

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В
 // 3, 5 -> 243 (3*)
 // 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите значение числа: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите в какую степень возводить: ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+
+double x = Math.Pow(a, b);
+
+Console.WriteLine(x);
