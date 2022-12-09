89d4808a005059c35e893651fa836b831c9467af
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-09 15:19:19 +0300

    mistakes

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,3 +3,20 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Clear();
+Console.WriteLine("Enter number = ");
+
+string num = Console.ReadLine();
+int len = num.Length;
+int[] nums = new int[len];
+int sum = 0;
+int count = 0;
+while (count < len)
+{
+    nums[count] = Convert.ToInt32(num[count].ToString()); // перевести в отдельные значения
+    count++;
+}
+for (int i = 0; i < len; i++)
+    sum += nums[i];
+
+Console.WriteLine(sum);
